662c4a956e64b786d62fc64f2ce63704e238ec43
Kenji Rikitake <kenji.rikitake@acm.org>
2021-12-07 14:22:16 +0900

    fix: add table headers in aggregate.sh

--- aggregated-values/aggregate.sh
+++ aggregated-values/aggregate.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-cat ../measured-values/JJ1BDX_Z_*-freq.txt | awk 'NR > 4' | sort -n > jj1bdx-eclipse-202112-freq.txt
-cat ../measured-values/JJ1BDX_Z_*-ampl.txt | awk 'NR > 4' | sort -n > jj1bdx-eclipse-202112-ampl.txt
+echo "Time Freq" > jj1bdx-eclipse-202112-freq.txt
+cat ../measured-values/JJ1BDX_Z_*-freq.txt | awk 'NR > 4' | sort -n >> jj1bdx-eclipse-202112-freq.txt
+#
+echo "Time Level" > jj1bdx-eclipse-202112-ampl.txt
+cat ../measured-values/JJ1BDX_Z_*-ampl.txt | awk 'NR > 4' | sort -n >> jj1bdx-eclipse-202112-ampl.txt
