278fcde132f9cda61ab31f155e633ade315ecbdc
Kenji Rikitake <kenji.rikitake@acm.org>
2021-12-07 17:44:35 +0900

    feat: add WAV file tagging examples

---
+++ add-wavfile-tags/README.md
@@ -0,0 +1,7 @@
+# For handling WAV file tags
+
+* Use [taglib](https://github.com/taglib/taglib)
+* Build tagreader and tagwriter binaries
+* see `add-tag-template.sh` for tagging a file
+* see `tag-adding-example.sh.txt` for a tagging example 
+
---
+++ add-wavfile-tags/add-tag-template.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+tagwriter  \
+  -R "title" "JJ1BDX_Z_9999kHz_2021-MM-DD_HH-MM-SS" \
+  -R "artist" "JJ1BDX" \
+  -R "album" "December 2021 Eclipse" \
+  -R "year" "2021" \
+  -R "comment" "" \
+  -R "track" "1" \
+  -R "genre" "Grape 1 with GPSDO" \
+  -R "AGC" "Off" \
+  -R "ALBUM" "December 2021 Eclipse" \
+  -R "ANTENNA" "1m-length 30m-band whip at 8m height above the ground" \
+  -R "COUNTRY" "Japan" \
+  -R "DATE" "2021" \
+  -R "ELEVATION_(M)" "Put elevation here" \
+  -R "EMAIL" "PUT EMAIL HERE" \
+  -R "FREQUENCY" "10 MHz" \
+  -R "GENRE" "Grape 1 with GPSDO" \
+  -R "GPSDO" "Yes" \
+  -R "GRID SQUARE" "PM95tp" \
+  -R "LATITUDE" "Put latitude here" \
+  -R "LONGITUDE" "Put longitude here" \
+  -R "RIG" "Grape 1" \
+  -R "SOUND_CARD" "Sabrent Audio Interface" \
+  -R "TIME ZONE" "UTC +00:00" \
+  -R "TITLE" "JJ1BDX_Z_9999kHz_2021-MM-DD_HH-MM-SS" \
+  -R "TRACKNUMBER"   "1" \
+  $1
