47e4e5dede95acbf1cfc73b65150e78bb17dc760
Kenji Rikitake <kenji.rikitake@acm.org>
2021-12-07 14:52:53 +0900

    fix: include 20211206 data to builddata.R and dateplot.R

--- aggregated-values/builddata.R
+++ aggregated-values/builddata.R
@@ -10,13 +10,16 @@ freqfun = stats::approxfun(freqdata$Time, freqdata$Freq)
 levelfun = stats::approxfun(ampldata$Time, ampldata$Level)
 # UNIX Time:
 # 1-DEC-2021 0000UTC -> TZ=UTC date -j 1201000021 "+%s" -> 1638316800
-# 6-DEC-2021 0000UTC -> TZ=UTC date -j 1206000021 "+%s" -> 1638748800
+# 7-DEC-2021 0000UTC -> TZ=UTC date -j 1206000021 "+%s" -> 1638835200
 # 11-DEC-2021 0000UTC -> TZ=UTC date -j 1211000021 "+%s" -> 1639180800
-# Smoothing for 20 secods
-smoothtime=c(1638316800:1638748800)
+starttime=1638316800
+endtime=1638835190
+timepair=c(starttime:endtime)
+# Smoothing for 20 seconds
+smoothtime=timepair
 smoothfreq=SMA(freqfun(smoothtime),20)
 smoothlevel=SMA(levelfun(smoothtime),20)
 # Make interpolation functions for the smoothed data
-smoothfuntime=c(1638316800:1638748800)
+smoothfuntime=timepair
 smoothfreqfun=stats::approxfun(smoothtime,smoothfreq)
 smoothlevelfun=stats::approxfun(smoothtime,smoothlevel)
